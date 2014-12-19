b26d4175d1332ba621216ffff01a50baf373ea31
moduleGit <chrisnevelsteen@gmail.com>
2014-12-19 21:00:49 +0100

    hallo allemaal

--- src/gitcursus/GitCursus.java
+++ src/gitcursus/GitCursus.java
@@ -17,7 +17,7 @@ public class GitCursus {
     public static void main(String[] args) {
 
         System.out.println("Bonjour");
-        System.out.println("Hallo");
+        System.out.println("Hallo allemaal");
     }
     
 }
